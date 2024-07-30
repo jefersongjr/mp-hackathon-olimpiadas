e851ac711295510b208cba5c6f974632213b6265
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-30 18:34:04 -0300

    feat :sparkles: criando tipo event

---
+++ front-end/src/app/data/types/types.ts
@@ -0,0 +1,10 @@
+export interface Event {
+    id: number;
+    event_name: string;
+    discipline_name: string;
+    start_date: string;
+    end_date: string;
+    venue_name: string;
+    discipline_pictogram: string;
+  }
+  
