5e1568f9e015d179550c7da99061d812a9c25937
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-30 17:40:13 -0300

    feat :sparkles: buscando dados da api

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['codante.s3.amazonaws.com'],
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'https://apis.codante.io/olympic-games/:path*',
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
